5bab6a16a00f1bf78e626e2bd8b75752d51726ea
PiSquared <krakatoachris@gmail.com>
2022-03-03 01:11:51 +0000

    I did a thing

--- src/com/alphander/Main.java
+++ src/com/alphander/Main.java
@@ -7,11 +7,10 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		String str = "{\"test\" : true}";
+		String str = "{\"test\" : [\"tat\", 0124.125, false], \"brbd\" : []}";
 		Parcer p = new Parcer();
 		Part root = p.load(str);
 		String out = p.save(root);
-		System.out.print(out);
 		System.out.print(out);
 	}
 }
